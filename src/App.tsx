620f3f3d102bf7be616a262d7ebeb4ac1527797e
Moni <thaker.moni@gmail.com>
2025-01-05 01:00:54 +1300

    feat: setup react-router and create/join pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { NavLink } from 'react-router'
 import './App.css'
 
 function App() {
-
   return (
     <div className='h-screen flex flex-col justify-center items-center'>
       <img src='/spy_icon.svg' className='w-48 h-48'></img>
       <h1 className='text-6xl font-bold py-16'>Spyfall</h1>
       <div className='flex justify-items-center flex-col w-28 space-y-4 mx-auto'>
-        <button className='border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>Create Game</button>
-        <button className='border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>Join Game</button>
+        <NavLink to='/create'>
+          <button className='w-full border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>
+            Create Game
+          </button>
+        </NavLink>
+        <NavLink to='/join'>
+          <button className='w-full border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>
+            Join Game
+          </button>
+        </NavLink>
       </div>
     </div>
   )
---
+++ src/pages/CreateGame.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router'
+import '../App.css'
+
+function CreateGame() {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <img src='/spy_icon.svg' className='w-48 h-48'></img>
+      <h1 className='text-6xl font-bold py-16'>Spyfall</h1>
+      <div className='flex flex-col justify-center w-40 pb-8 space-y-4 mx-auto'>
+        <p className='text-2xl text-white'>Enter Name:</p>        
+        <input></input>        
+      </div>
+      <div className='flex justify-items-center flex-col w-28 space-y-4 mx-auto'>
+        <button className='w-full border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>
+            Next
+        </button>
+        <NavLink to='/'>
+            <button className='w-full border rounded-md p-1.5 border-slate-400 bg-slate-300 hover:bg-slate-500 hover:text-white'>
+                Back
+            </button>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default CreateGame
